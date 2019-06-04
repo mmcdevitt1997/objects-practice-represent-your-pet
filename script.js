efb9e9be8195ab99111c1f58da1655f80bb74671
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-06-04 10:49:55 -0500

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+const reganTheDog = {
+    name: "Regan",
+    species: "mut", 
+    nicknames: ["white","girl"], 
+    age: 3, 
+}
